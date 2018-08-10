a4e477e9a5c9bd82938cec07081ff7af2256f070
yashpal13108 <yashpal13108@IND-MHPDW100279>
2018-08-10 18:33:17 +0530

    Issue resolution

--- MarketSummaryWeb/Global.asax.cs
+++ MarketSummaryWeb/Global.asax.cs
@@ -18,7 +18,7 @@ namespace MarketSummaryWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            CosmosRepository<ProspectSearchCriteria>.Initialize();
+            //CosmosRepository<ProspectSearchCriteria>.Initialize();
         }
     }
 }
